a79791f928d52f1028034686da2e34b51a50d764
wangxinxin <wangxinxin@sensetime.com>
2023-06-05 17:19:42 +0800

    fix

--- pages/PaymentStatus.jsx
+++ pages/PaymentStatus.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {useStripe} from '@stripe/react-stripe-js';
 import {loadStripe} from '@stripe/stripe-js';
 import {Elements} from '@stripe/react-stripe-js';
 import Link from 'next/link'; // Import Link component from Next.js
