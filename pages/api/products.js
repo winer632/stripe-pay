7b0da8e1a17426eb690b0814770b8bb00f15e8d8
wangxinxin <wangxinxin@sensetime.com>
2023-06-05 20:22:20 +0800

    add Choose a product page

---
+++ pages/api/products.js
@@ -0,0 +1,23 @@
+// This is your test secret API key. 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export default async function handler(req, res) { 
+    // List products from Stripe 
+    const products = await stripe.products.list(); 
+    // Map products to include prices 
+    const productsWithPrices = await Promise.all( 
+        products.data.map(
+            async (product) => { 
+                // Get the first price for each product 
+                const price = await stripe.prices.list({ product: product.id, limit: 1, }); 
+                return { 
+                    product, 
+                    price: price.data[0].unit_amount, 
+                    currency: price.data[0].currency, 
+                }; 
+            }
+        ) 
+    ); 
+    
+    res.send(productsWithPrices); 
+};
--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,64 @@
-import React from "react";
-import { loadStripe } from "@stripe/stripe-js";
+import React from "react"; 
+import { loadStripe } from "@stripe/stripe-js"; 
 import { Elements } from "@stripe/react-stripe-js";
 
 import CheckoutForm from "../components/CheckoutForm";
 
-// Make sure to call loadStripe outside of a component’s render to avoid
-// recreating the Stripe object on every render.
-// This is your test publishable API key.
+// This is your test publishable API key. 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function App() {
-  const [clientSecret, setClientSecret] = React.useState("");
+    const [products, setProducts] = React.useState([]); 
+    const [selectedProduct, setSelectedProduct] = React.useState(null); 
+    const [clientSecret, setClientSecret] = React.useState("");
 
-  React.useEffect(() => {
-    // Create PaymentIntent as soon as the page loads
-    fetch("/api/create-payment-intent", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
-    })
-      .then((res) => res.json())
-      .then((data) => setClientSecret(data.clientSecret));
-  }, []);
+    React.useEffect(() => { 
+        // Fetch products from the server 
+        fetch("/api/products") .then((res) => res.json()) .then((data) => setProducts(data)); }, []);
 
-  const appearance = {
-    theme: 'stripe',
-  };
-  const options = {
-    clientSecret,
-    appearance,
-  };
+    const handleSelectProduct = (product) => { 
+        // Set the selected product 
+        setSelectedProduct(product); 
+        // Create a PaymentIntent with the product price and currency 
+        fetch(
+            "/api/create-payment-intent", 
+            { 
+                method: "POST", 
+                headers: { "Content-Type": "application/json" }, 
+                body: JSON.stringify({ product }), 
+            }
+        ) .then((res) => res.json()) .then((data) => setClientSecret(data.clientSecret)); };
 
-  return (
-    <div className="App">
-      {clientSecret && (
-        <Elements options={options} stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      )}
-    </div>
-  );
+    const appearance = { theme: 'stripe', }; 
+    const options = { clientSecret, appearance, };
+    
+    
+
+    return ( 
+        <div className="App"> 
+            <h1>Choose a product</h1> 
+            <div className="products"> 
+                {products.map((product) => ( 
+                    <div key={product.product.id} className="product"> 
+                        <h2>{product.product.name}</h2> 
+                        <p>
+                            {product.product.description} {product.price / 100}{" "}
+                            {product.currency === "cny"
+                                ? "人民币"
+                                : product.currency === "hkd"
+                                ? "港币"
+                                : product.currency === "usd"
+                                ? "美元"
+                                : product.currency === "eur"
+                                ? "欧元"
+                                : product.currency}
+                        </p>
+
+                        <p></p> 
+                        <button onClick={() => handleSelectProduct(product)}>Proceed</button> 
+                    </div> ))} 
+            </div> 
+            {clientSecret && selectedProduct && ( <Elements options={options} stripe={stripePromise}> <CheckoutForm product={selectedProduct} /> </Elements> )} 
+        </div> ); 
+    
 }
