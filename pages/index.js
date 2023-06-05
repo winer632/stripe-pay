10c81cd03418b8770f4cc6cc722d096925e67266
wangxinxin <wangxinxin@sensetime.com>
2023-06-05 20:41:05 +0800

    modify page style

--- pages/index.js
+++ pages/index.js
@@ -1,64 +1,93 @@
-import React from "react"; 
-import { loadStripe } from "@stripe/stripe-js"; 
+import React from "react";
+import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
+import { useRouter } from "next/router";
 
 import CheckoutForm from "../components/CheckoutForm";
 
-// This is your test publishable API key. 
+// This is your test publishable API key.
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
 
 export default function App() {
-    const [products, setProducts] = React.useState([]); 
-    const [selectedProduct, setSelectedProduct] = React.useState(null); 
-    const [clientSecret, setClientSecret] = React.useState("");
+  const [products, setProducts] = React.useState([]);
+  const [selectedProduct, setSelectedProduct] = React.useState(null);
+  const [clientSecret, setClientSecret] = React.useState("");
 
-    React.useEffect(() => { 
-        // Fetch products from the server 
-        fetch("/api/products") .then((res) => res.json()) .then((data) => setProducts(data)); }, []);
+  // Use Next.js router to get the product id from the query string
+  const router = useRouter();
+  const { productId } = router.query;
 
-    const handleSelectProduct = (product) => { 
-        // Set the selected product 
-        setSelectedProduct(product); 
-        // Create a PaymentIntent with the product price and currency 
-        fetch(
-            "/api/create-payment-intent", 
-            { 
-                method: "POST", 
-                headers: { "Content-Type": "application/json" }, 
-                body: JSON.stringify({ product }), 
-            }
-        ) .then((res) => res.json()) .then((data) => setClientSecret(data.clientSecret)); };
+  React.useEffect(() => {
+    // Fetch products from the server
+    fetch("/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
-    const appearance = { theme: 'stripe', }; 
-    const options = { clientSecret, appearance, };
-    
-    
+  React.useEffect(() => {
+    // If there is a product id in the query string, find the matching product
+    if (productId) {
+      const product = products.find((p) => p.product.id === productId);
+      if (product) {
+        // Set the selected product
+        setSelectedProduct(product);
+        // Create a PaymentIntent with the product price and currency
+        fetch("/api/create-payment-intent", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ product }),
+        })
+          .then((res) => res.json())
+          .then((data) => setClientSecret(data.clientSecret));
+      }
+    }
+  }, [productId, products]);
 
-    return ( 
-        <div className="App"> 
-            <h1>Choose a product</h1> 
-            <div className="products"> 
-                {products.map((product) => ( 
-                    <div key={product.product.id} className="product"> 
-                        <h2>{product.product.name}</h2> 
-                        <p>
-                            {product.product.description} {product.price / 100}{" "}
-                            {product.currency === "cny"
-                                ? "人民币"
-                                : product.currency === "hkd"
-                                ? "港币"
-                                : product.currency === "usd"
-                                ? "美元"
-                                : product.currency === "eur"
-                                ? "欧元"
-                                : product.currency}
-                        </p>
+  const handleSelectProduct = (product) => {
+    // Redirect to a new page with the product id as a query parameter
+    router.push(`/?productId=${product.product.id}`);
+  };
 
-                        <p></p> 
-                        <button onClick={() => handleSelectProduct(product)}>Proceed</button> 
-                    </div> ))} 
-            </div> 
-            {clientSecret && selectedProduct && ( <Elements options={options} stripe={stripePromise}> <CheckoutForm product={selectedProduct} /> </Elements> )} 
-        </div> ); 
-    
+  const appearance = { theme: "stripe" };
+  const options = { clientSecret, appearance };
+
+  return (
+    <div className="App">
+      {/* Only show the h1 element when there is no product id in the query string */}
+      {!productId && <h1>Choose a product</h1>}
+      {/* Only show the products list and their proceed buttons when there is no product id in the query string */}
+      {!productId && (
+        <div className="products">
+          {products.map((product) => (
+            <div key={product.product.id} className="product">
+              <h2>{product.product.name}</h2>
+              <p>
+                {product.product.description} {product.price / 100}{" "}
+                {product.currency === "cny"
+                  ? "人民币"
+                  : product.currency === "hkd"
+                  ? "港币"
+                  : product.currency === "usd"
+                  ? "美元"
+                  : product.currency === "eur"
+                  ? "欧元"
+                  : product.currency}
+              </p>
+
+              <p></p>
+              <button onClick={() => handleSelectProduct(product)}>
+                Proceed
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+      {clientSecret && selectedProduct && (
+        <Elements options={options} stripe={stripePromise}>
+          <CheckoutForm product={selectedProduct} />
+        </Elements>
+      )}
+    </div>
+  );
+
 }
