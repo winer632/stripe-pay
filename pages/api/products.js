5974560ca8547dc18ae7622e6629570211f7b746
wangxinxin <winer632@qq.com>
2023-06-17 10:33:48 +0800

    only display activated products

--- pages/api/products.js
+++ pages/api/products.js
@@ -3,7 +3,7 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) { 
     // List products from Stripe 
-    const products = await stripe.products.list(); 
+    const products = await stripe.products.list({ active: true }); 
     // Map products to include prices 
     const productsWithPrices = await Promise.all( 
         products.data.map(
